c30ef601cde54f94f4e51a8f5aa591a9ab2c8f65
tooscl <ia-babkn@yandex.ru>
2025-03-07 21:26:15 +0300

    bot.py v0.1 — Настрйока работы с сообщениями

--- app/telegram/bot.py
+++ app/telegram/bot.py
@@ -1,13 +1,32 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 
 from app.config import TOKEN
+
+import app.telegram.messages as msg
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+# Работа с командами
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer(msg.start)
+
+@dp.message(Command("help"))
+async def cmd_start(message: types.Message):
+    await message.answer(msg.help)
+
+@dp.message(Command("restart"))
+async def cmd_start(message: types.Message):
+    await message.answer(msg.restart)
+
+@dp.message(Command("feedback"))
+async def cmd_start(message: types.Message):
+    await message.answer(msg.feedback)
 
 @dp.message()
 async def echo(message: types.Message):
