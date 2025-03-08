5392f710b86e3df310c8456ea9d064df7e02214f
tooscl <ia-babkn@yandex.ru>
2025-03-08 10:11:56 +0300

    v0.3.1 - Изменение структуры репозитория

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
-from backend.config import TOKEN
+from config import TOKEN
 
 import bot.messages as msg
 
