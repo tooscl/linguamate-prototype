73864bf1e2fd19556f313a6fb51127baaa8c8f56
tooscl <ia-babkn@yandex.ru>
2025-03-13 10:02:41 +0300

    v1.2 Улучшение промпта

--- bot/middlewares/get_context.py
+++ bot/middlewares/get_context.py
@@ -7,5 +7,4 @@ from bot.utils.context_to_dialogue import context_to_dialogue
 async def get_context(message: types.Message):
     async with AsyncSessionLocal() as db:
         context = context_to_dialogue(await get_messages(db, message.from_user.id))
-        print(context)
     return context
